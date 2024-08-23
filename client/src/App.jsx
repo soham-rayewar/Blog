9d087eeeaba6aab69fd15aec3811890b8e884a3c
soham <soham9822@gmail.com>
2024-08-23 23:50:24 +0530

    install react js and tailwind css and created first template

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/no-unescaped-entities */
+
+import React from "react";
+
+export default function App() {
+  return (
+    <div>
+      <h1 className="text-4xl text-red-600 font-bold underline">
+        Soham's blogs
+      </h1>
+    </div>
+  );
+}
