8a6e25adf37ff15d9b4a63436e6a460bd8d59efa
soham <soham9822@gmail.com>
2024-08-27 15:39:57 +0530

    Create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,28 @@
 /* eslint-disable react/no-unescaped-entities */
 
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
 
+import Projects from "./pages/Projects";
+import Signin from "./pages/Signin";
+import Signup from "./pages/signup";
+import Dashboard from "./pages/Dashboard";
 export default function App() {
   return (
     <div>
-      <h1 className="text-4xl text-red-600 font-bold underline">
-        Soham's blogs
-      </h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/About" element={<About />} />
+
+          <Route path="/Dashboard" element={<Dashboard />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
